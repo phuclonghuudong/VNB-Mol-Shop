557a4aeea0d589878cac44c455d401c8cb6d2180
phuc <phucpham.241099@gmail.com>
2025-08-03 01:07:47 +0700

    fix F5 run vercel

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ DỰ ÁN VNB-SHOP
 
 KHỞI CHẠY DỰ ÁN: npm i -> npm start
 Start: 05/07/2025
+
+https://vnb-mol-shop.vercel.app/
--- shop-app/src/v1/screens/auth/AuthLayout.jsx
+++ shop-app/src/v1/screens/auth/AuthLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import TitleCategoryList from "../../components/ui/TitleCategoryList";
 
 const AuthLayout = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center backgroundImageAccount p-2">
+    <div className="min-h-screen flex flex-col items-center justify-center backgroundImageAccount p-2 gap-5">
       <div className="w-full max-w-lg p-6 bg-white rounded shadow ">
         <Outlet />
+      </div>
+      <div className="w-full max-w-lg p-6 bg-white rounded shadow ">
+        <Link to={"/"}>
+          <TitleCategoryList title={"Molxipi shop  "} isUppercase />
+        </Link>
       </div>
     </div>
   );

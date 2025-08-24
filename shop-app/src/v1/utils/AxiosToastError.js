f4a461bcd52f3610c48df828e85b1135ebf9099f
phuc <72928119+phuclonghuudong@users.noreply.github.com>
2025-08-24 23:44:57 +0700

    Update AxiosToastError.js

--- shop-app/src/v1/utils/AxiosToastError.js
+++ shop-app/src/v1/utils/AxiosToastError.js
@@ -2,7 +2,8 @@ import toast from "react-hot-toast";
 
 const AxiosToastError = (error) => {
   console.log("LỖI AxiosToastError: ", error);
-  toast.error(error?.response?.data?.MESSAGE || error?.response?.statusText);
+  toast.error(error?.response?.data?.MESSAGE || error?.response?.statusText || error?.message);
 };
 
 export default AxiosToastError;
+
